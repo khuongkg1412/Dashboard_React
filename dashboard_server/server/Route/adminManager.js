18daf1f403ee95157c92f6811ec509de176633e3
HaThucHuynhDuy <52154699+davidduy@users.noreply.github.com>
2022-04-14 19:59:30 +0700

    xong hien bang admin

---
+++ dashboard_server/server/Route/adminManager.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("../config");
+var router = express.Router();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const AdminDB = db.collection("Admin");
+const AdminModel  = require("../Model/admin");
+
+/* GET users listing. */
+router.get("/", async (req, res) => {
+    const data = await AdminDB.get();
+    const arrayAdmin = [];
+    if(data.empty){
+      
+      res.status(404).send("No admin in list");
+    }else{console.log("lalalalala");
+      data.forEach(element => {
+        var admin = new AdminModel(
+          element.data().Avatar,
+          element.data().Email,
+          element.data().Phone,
+          element.data().Username,
+          element.data().status
+        );
+        arrayAdmin.push(admin);
+      });
+    }
+    res.send(arrayAdmin);
+  });
+
+module.exports = router;
--- dashboard_server/server/Route/index.js
+++ dashboard_server/server/Route/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const db = require("./config");
+const db = require("../config");
 
 const app = express();
 app.use(express.json());
